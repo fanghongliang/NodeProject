ccd852074f9778bcdd0c86822e5c46ae88e706b9
fanghongliang <18292656094@163.com>
2020-04-21 11:56:53 +0800

    feat: index路由查询逻辑

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,15 +1,24 @@
 var express = require('express');
 var router = express.Router();
-
+let db = require('../db')
+const {log} = console
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
 router.get('/user', (req, res, next) => {
-  res.json({
-    status: 600,
-    mag: {name: 'fhl', age: '18'}
+  const sql = 'select * from user'
+  db.query(sql, [], function(result, fields) {
+    log('查询数据', result)
+    let data = JSON.parse(JSON.stringify(result))
+    if(data[0].address = '北京') {
+      data[0].address = '上海'
+    }
+    res.json({
+      status: 0,
+      data,
+    })
   })
 })
 
