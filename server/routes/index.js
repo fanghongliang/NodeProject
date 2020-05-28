4bf4ab90ceef3a6b986ba0621a708bc85cbc85c3
fanghongliang <18292656094@163.com>
2020-05-28 14:15:42 +0800

    feat: api路由

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,7 +1,73 @@
 var express = require('express');
 var router = express.Router();
 let db = require('../db')
+var {Message, User} = require('../models/test.js')
+const jwt = require('jsonwebtoken')
 const {log} = console
+
+//登录
+router.get('/login', (req, res, next) => {
+  const user = {}
+  const user_id = ''
+  const user_data = ''
+  User.create(user).then(data => {
+    // user_id = data.id\
+    console.log('生成新用户id----------', data.id)
+    user_data = data
+  })
+  const token = 'Bearer ' + jwt.sign(
+    {
+      id: user_data.id,
+    },
+    'secret123456',
+    {
+      expiresIn: 3600 * 24 * 7
+    }
+  )
+  res.json({
+    errcode: 0,
+    data: {
+      data: user_data,
+      token
+    }
+  })
+})
+
+//添加一条留言
+router.post('/add_msg', (req, res, next) => {
+  var {userName, content} = req.body
+  if(!userName || !content) {
+    res.json({
+      errcode: 600,
+      desc: '参数无效'
+    })
+    return
+  }
+  var message = {
+    userName: req.body.userName,
+    content: req.body.content,
+  }
+  Message.create(message).then(data => {
+    console.log('插入数据res', data)
+    res.json({data})
+  }).catch(err => {
+    console.log('报错err', err)
+  }) 
+})
+
+//查找某内容
+router.get('/getOne', (req, res, next) => {
+  Message.findAll().then(data => {
+    console.log('查找数据res', res)
+    res.json({
+      errcode: 400,
+      data
+    })
+  })
+})
+
+
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
@@ -14,18 +80,50 @@ router.get('/user', (req, res, next) => {
     let data = JSON.parse(JSON.stringify(result))
     if(data[0].address = '北京') {
       data[0].address = '上海'
+
     }
+    data1 = req.requestTime
     res.json({
       status: 0,
       data,
+      data1
     })
   })
 })
+
+function logOriginalUrl (req, res, next) {
+  console.log('Request URL:', req.originalUrl)
+  next()
+}
+
+function logMethod (req, res, next) {
+  console.log('Request Type:', req.method)
+  next()
+}
 
 router.get('/email', (req, res, next) => {
   res.json({
     status: 601,
     mag: {name: 'fhl', email: '127277'}
+  })
+})
+
+var logStuff = [logOriginalUrl, logMethod]
+router.get('/register', logStuff, (req, res, next) => {
+  let params = req.query
+  console.log('post 参数', params)
+  res.json({
+    errcode: 0,
+    data: {'user': '0'}
+  })
+})
+
+router.get('/register', (req, res, next) => {
+  let params = req.query
+  console.log('post 参数2', params)
+  res.json({
+    errcode: 0,
+    data: {'user': '1'}
   })
 })
 
--- server/routes/users.js
+++ server/routes/users.js
@@ -1,9 +1,70 @@
 var express = require('express');
+var request = require('request');
 var router = express.Router();
 
+router.get('/user', (req, res, next) => {
+  res.json({
+    "name": 'ffff'
+  })
+})
+
+const wxConfig = {
+  'AppID': 'wxc5cfc90347a82cfb',
+  'AppSecret': '9bcb7f26a0c8104869d55dc9537063b2'
+}
+
+//生成token
+function createToken() {
+  const chars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+  const length = chars.length;
+  let str = "";
+  for (let i = 0; i < length; i++) {
+    str += chars.substr(Math.round(Math.random() * length), 1);
+  }
+  return str;
+};
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.post('/login', function(req, res, next) {
+  var code = req.query.code;
+  console.log('接收到code:12',)
+  res.json({
+    "name": 'token'
+  })
+  // request.get({
+  //   url: 'https://api.weixin.qq.com/sns/jscode2session',
+  //   json: true,
+  //   qs: {
+  //     grant_type: 'authorization_code',
+  //     appid: wxConfig.AppID,
+  //     secret: wxConfig.AppSecret,
+  //     js_code: code
+  //   }
+  // }, function (err, res, data) {
+  //   if (res.statusCode === 200) {
+  //     console.log('登录成功返回数据： ', data)
+  //     // User.findOne({
+  //     //   openid: data.openid
+  //     // }).then(function (info) {
+  //     //   if (info) {
+  //     //     console.info('用户已经存在');
+  //     //     console.info(info.token)
+  //     //     return res.send(token);
+  //     //   } else {
+  //     //     var user = new User({
+  //     //       openid: data.openid,
+  //     //       nickName: param.nickname,
+  //     //       avatarUrl: param.head_img,
+  //     //       creatAt: param.creatAt,
+  //     //       token: createToken()
+  //     //     })
+  //     //     return user.save();
+  //     //   }
+  //     // })
+  //   } else {
+  //     console.info('[error]', err)
+  //     res.json(err);
+  //   }
+  // })
 });
 
 module.exports = router;
