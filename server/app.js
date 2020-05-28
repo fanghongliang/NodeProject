f93ea3340ab08a0e59f79d0f9d60a3b1efab468f
fanghongliang <18292656094@163.com>
2020-05-28 14:09:34 +0800

    chore: 文件配置

--- server/app.js
+++ server/app.js
@@ -2,26 +2,47 @@ var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
+var bodyParse = require('body-parser');
 var logger = require('morgan');
+const expressJwt = require('express-jwt')
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
 var app = express();
 
+//中间件
+const requestTime = function(req, res, next) {
+  req.requestTime = Date.now()
+  next()
+}
+
+// 托管静态文件
+// app.use('/static', express.static('public'))                          //相对路径
+app.use('/static', express.static(path.join(__dirname, 'public')))       //绝对路径
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
+app.set('json spaces', 2)
+app.use(requestTime)
+
 app.use(logger('dev'));
+app.use(bodyParse.json()) 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/v2', usersRouter);
 
+app.use(expressJwt({
+  secret: 'secret123456'  // 签名的密钥 或 PublicKey
+}).unless({
+  path: ['/login', '/getOne']  // 指定路径不经过 Token 解析
+}))
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
